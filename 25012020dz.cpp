a589f207c9fc3d983a5d2fee974afdcfa9440950
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 19:29:54 +0300

    Добавьте файлы проекта.

---
+++ 25012020dz.cpp
@@ -0,0 +1,24 @@
+﻿/*
+уравнение, формула
+*/
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	double a, b, x, y;
+	cout << "Введите a: ";
+	cin >> a;
+	cout << "Введите b: ";
+	cin >> b;
+	cout << "Введите x: ";
+	cin >> x;
+	if (pow(b, 2) > a * x)
+		y = exp(sin(x)) + b * sqrt(2 * cos(6 * x - 0.3));
+	else if ((pow(b, 2) <= a * x))
+		y = exp(-x) + sqrt(tan(abs(3 * x + 0.6)));
+	cout << "y = " << y << endl;
+	return 0;
+}
